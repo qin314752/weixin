15644ef99c5c6da88eab581dabbcb2005df849d4
秦中原 <qin314752@163.com>
2016-11-30 12:41:38 +0800

    f

---
+++ weixin/index.php
@@ -0,0 +1,26 @@
+<?php 
+echo 1;
+	// header('content-type:text');
+	// define("TOKEN", "qin123");
+	
+	// // header('Content-Type: text/html');
+	// include './wx_sample.php';
+
+	// $wechatObj = new wechatCallbackapiTest();
+	// // $wechatObj->valid();
+	// $wechatObj->responseMsg();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
